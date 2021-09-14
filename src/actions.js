03c40e36c271d0f623795a2685ea3db8df50cc4c
cassidypignatello <cassidy.pignatello@gmail.com>
2021-09-14 20:01:21 +0800

    adds payloads for max and min values

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,12 @@
+const maxValue = 100;
+const minValue = 0;
+
 export const incrementAction = {
   type: 'increment',
+  payload: {maxValue},
 };
 
 export const decrementAction = {
   type: 'decrement',
+  payload: {minValue},
 };
