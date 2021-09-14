5b425b018288003dafe3631894c0ceacca72d70b
cassidypignatello <cassidy.pignatello@gmail.com>
2021-09-14 19:36:43 +0800

    fixes reducers

--- src/reducer.js
+++ src/reducer.js
@@ -7,13 +7,13 @@ export default function appReducer(state = initialState, action) {
     case 'increment': {
       return {
         ...state,
-        ...{progress: state + 20},
+        progress: state.progress + 20,
       };
     }
     case 'decrement': {
       return {
         ...state,
-        ...{progress: state - 20},
+        progress: state.progress - 20,
       };
     }
     default:
